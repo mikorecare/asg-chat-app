d9fa88255d36cc1f8cd111c78c862e59a05b7e2e
Miko Recare <mikorecare@gmail.com>
2023-02-09 05:55:22 +0800

    FEAT: Added register, edit-profile, and delete-profile components, bootstrap installation

--- asg-chat-app/src/app/app.module.ts
+++ asg-chat-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './pages/login/register/register.component';
+import { EditProfileComponent } from './pages/login/register/edit_profile/edit-profile/edit-profile.component';
+import { DeleteProfileComponent } from './pages/delete-profile/delete-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    EditProfileComponent,
+    DeleteProfileComponent
   ],
   imports: [
     BrowserModule,
---
+++ asg-chat-app/src/app/pages/delete-profile/delete-profile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-profile',
+  templateUrl: './delete-profile.component.html',
+  styleUrls: ['./delete-profile.component.scss']
+})
+export class DeleteProfileComponent {
+
+}
