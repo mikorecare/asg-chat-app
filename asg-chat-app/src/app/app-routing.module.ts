c9cf3c75731c03367ea102f85dd4c0d230e7f8af
Miko Recare <mikorecare@gmail.com>
2023-02-09 07:35:55 +0800

    FEAT: Login and registration Forms

--- asg-chat-app/src/app/app-routing.module.ts
+++ asg-chat-app/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './pages/register/register.component';
+import { EditProfileComponent } from './pages/edit-profile/edit-profile.component';
+import { DeleteProfileComponent } from './pages/delete-profile/delete-profile.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ 
+  { 
+    path: 'login', 
+    component: LoginComponent,
+    children: [
+     
+    ] 
+},
+{
+  path: 'register', 
+  component: RegisterComponent
+},
+  { path: 'edit-profile/:id', component: EditProfileComponent },
+  { path: 'delete-profile/:id', component: DeleteProfileComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- asg-chat-app/src/app/app.module.ts
+++ asg-chat-app/src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './pages/login/register/register.component';
-import { EditProfileComponent } from './pages/login/register/edit_profile/edit-profile/edit-profile.component';
+import { RegisterComponent } from './pages/register/register.component';
 import { DeleteProfileComponent } from './pages/delete-profile/delete-profile.component';
+import { EditProfileComponent } from './pages/edit-profile/edit-profile.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
     EditProfileComponent,
-    DeleteProfileComponent
+    DeleteProfileComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
