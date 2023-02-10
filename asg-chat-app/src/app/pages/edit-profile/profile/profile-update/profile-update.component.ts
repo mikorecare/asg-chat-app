8e1c6c95d98bdbb490f0a04922eb55164b1ebe79
Miko Recare <mikorecare@gmail.com>
2023-02-10 10:14:20 +0800

    FEAT: Profile info update and change password

--- asg-chat-app/src/app/pages/edit-profile/profile/profile-update/profile-update.component.ts
+++ asg-chat-app/src/app/pages/edit-profile/profile/profile-update/profile-update.component.ts
@@ -2,6 +2,7 @@ import { Component,NgZone } from '@angular/core';
 import { Validators, FormGroup, FormBuilder } from "@angular/forms";
 import { CrudService } from 'src/app/service/crud.service';
 import { Router } from '@angular/router';
+import { ProfileComponent } from '../profile.component';
 
 @Component({
   selector: 'app-profile-update',
@@ -9,17 +10,19 @@ import { Router } from '@angular/router';
   styleUrls: ['./profile-update.component.scss']
 })
 export class ProfileUpdateComponent {
+  changePassword = false;
+  changeInfo = true;
   User:any = []
   userId = localStorage.getItem("userId");
   form: FormGroup;
   constructor(
+    private profile: ProfileComponent,
     private crudService: CrudService,
     private ngzone: NgZone,
     private router: Router,
     public formBuilder: FormBuilder,
   ){
     this.form = this.formBuilder.group({
-
       lastName: [''],
       firstName: [''],
       password: [''],
@@ -27,11 +30,23 @@ export class ProfileUpdateComponent {
     })
   }
 
+  change(){
+    this.changePassword = !this.changePassword;
+    this.changeInfo = !this.changeInfo;
+  }
+  
   getUser(){
     
-    this.crudService.GetUser(this.userId).subscribe((data)=>{this.User = [data["firstName"],data["lastName"],data["username"]]})
+    this.crudService.GetUser(this.userId).subscribe((data)=>{this.User = [data["firstName"],data["lastName"],data["username"],data["password"]]})
    }
 
+   get lastName(){
+    return this.form.get("lastName");
+  } 
+
+  get firstName(){
+    return this.form.get("firstName");
+  } 
   get password() {
     return this.form.get("password");
   }
@@ -39,9 +54,31 @@ export class ProfileUpdateComponent {
     return this.form.get("confirmPassword");
   }
 
+  save(){
+    alert("Update succesful!");
+    this.crudService.updateUser(this.userId,this.form.value).subscribe();
+    this.profile.getUser();
+  }
+
   update(){
-    if(this.confirmPassword?.value != this.password?.value){
-      alert("Passwords didn't match");
+    if(this.changeInfo){
+      if(this.firstName?.value.length < 1 || this.firstName?.value.length < 1){
+        alert("First Name or Last Name is empty!");
+      }
+      else{
+        this.password?.setValue(this.User[3]);
+        this.save();
+      }
+    }
+    if(this.changePassword){
+      if(this.confirmPassword?.value != this.password?.value || this.confirmPassword?.value.length < 6 || this.password?.value.length < 6){
+        alert("Passwords must match and must be 6 characters long");
+      }
+      else{
+        this.firstName?.setValue(this.User[0]);
+        this.lastName?.setValue(this.User[1]);
+        this.save();
+      }
     }
 
   }
